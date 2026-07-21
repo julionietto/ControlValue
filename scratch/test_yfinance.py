357d68527262db8a9726a79bd75614e65ef255da
Julio <julionietto@gmail.com>
2026-07-20 22:35:01 -0300

    [v6.0.2] Aprimora visão de proventos, adiciona cálculo de resumo anual, expande testes de formatters e atualiza documentação de arquitetura.

--- scratch/test_yfinance.py
+++
@@ -1,6 +0,0 @@
-import yfinance as yf
-ticker = "IRDM11.SA"
-info = yf.Ticker(ticker).info
-print("SECTOR:", info.get('sector'))
-print("INDUSTRY:", info.get('industry'))
-print("LONG NAME:", info.get('longName'))
